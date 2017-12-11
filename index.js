d9a4d1cf8bfc4cfc59b44189db9b6baf237c6ff0
Ben Matare <ben.a.matare@gmail.com>
2017-12-11 06:55:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,39 +1,32 @@
 
-
-//Accepts an array (Dwarves) and outputs a string of numbered dwarves i.e.
-// 1.Doc 2.Dopey 3.Bashful 4.Grumpy
-// Have to take the array and convert it into a string, but before each index add
-// An incremently increasing variable? ++
-
-
 function dwarfRollCall(dwarves) {
   for (var i = 0; i < dwarves.length; i++) {
     var num = i + 1;
     dwarves[i] = num + '. ' + dwarves[i];
-  }
+  } // Edit the array to become i+1 + Dwarves+'space' (For however long the array is)
   return dwarves.join(" ").toString();
-}
 
-// Accepts an array (planeteerCalls) outputs a new array with each word capitilized and with an exclamation point at the end
 
 function summonCaptainPlanet(planeteerCalls){
   for (var i = 0; i < planeteerCalls.length; i++){
       planeteerCalls[i] = planeteerCalls[i].toUpperCase()+'!';
-      }
+    } // Edit the array's index to be ALL CAPS + !
     return planeteerCalls;
 }
 
 function longPlaneteerCalls(words) {
   for (var i = 0; i < words.length; i++) {
-    if (words[i].length <= 4) {
+    if (words[i].length <= 4) { // If the index's length is less than or equal to 4
       return false;
-    } else if (words[i].length >= 4) {
+    } else if (words[i].length >= 4) { // If the index's length is greater than or equal to 4
       return true;
     }
   }
 }
 
+
 function findTheCheese(foods) {
+
   for (var i = 0; i <foods.length; i++){
     if (foods[i] === 'cheddar') {
       return foods[i]
